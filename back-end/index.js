565e97032f8814d7dc6b0ba3b3d0b46c8e760e8c
Zachary Landry <landryzachary4@gmail.com>
2023-01-22 11:57:20 -0500

    working to get DB connected

--- back-end/index.js
+++ back-end/index.js
@@ -3,13 +3,19 @@ const express = require("express");
 require("dotenv").config();
 const PORT = process.env.PORT;
 const app = express();
+const { Sequelize } = require('sequelize')
 
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 
+
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use('/marketplace', require('./controllers/marketplace'))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
 
 app.get('/', (req, res) => {
   res.render('Home');
@@ -20,6 +26,7 @@ app.get('/', (req, res) => {
 app.get('*', (req,res) => {
   res.status(404).send('<h1>404: PAGE NOT FOUND</h1>')
 })
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`)
