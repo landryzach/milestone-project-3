a3389df42568592c00da4a9357454ac37ea828ce
Zachary Landry <landryzachary4@gmail.com>
2023-01-24 22:55:42 -0500

    updated app.js; stuck on authentication

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,28 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './views/Home'
 import NewListing from './views/NewListing'
-//import EditListing from './views/EditListing'
-
+import EditListing from './views/EditListing'
+import Register from './views/Register'
+import SignIn from './views/SignIn'
+import marketplace from './controllers/marketplace'
 
 function App() {
-  return (      
+  return ( 
+          <BrowserRouter>     
            <Routes>
             <Route path="/" element={<Home />} />
-                
+            <Route path="/Sign-In" element={<SignIn />} />
+            <Route path="/Register" element={<Register />}/>
+            <Route path="/marketplace" element={<marketplace />}/>  
             <Route path="/NewListing" element={<NewListing />} />
-                
+            <Route path="/EditListing" element={<EditListing />} />   
             </Routes>
+          </BrowserRouter>
   );
 }
 
 export default App;
 
 
-//<Route path="/Sign-In" element={<Sign-In />} />
-//<Route path="/Register" element={<Register />}/>
-//<Route path="/edit/:id" element={<EditListing />} />
+
